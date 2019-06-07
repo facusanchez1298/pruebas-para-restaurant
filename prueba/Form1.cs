9dd1d8c03576ffdd4a997aec0220039841cbbead
Facundo Sanchez <fas_facu@hotmail.com>
2019-06-06 23:21:18 -0300

    ya arrastramos

--- prueba/Form1.cs
+++ prueba/Form1.cs
@@ -12,9 +12,34 @@ namespace prueba
 {
     public partial class Form1 : Form
     {
+        bool precionado = false;
+        Point inicial;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            precionado = true;
+            inicial = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            precionado = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Control control = (Control) sender;
+            if (precionado)
+            {
+                control.Top += e.Y - inicial.Y;
+                control.Left += e.X - inicial.X;
+            }
         }
     }
 }
